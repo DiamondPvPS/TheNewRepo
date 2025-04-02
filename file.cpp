ee7d409729a0d17db06d24f3e347bfe4731ab02f
DiamondPvPS <ykhangura13@gmail.com>
2025-04-02 19:17:26 -0400

    Added some changes

--- file.cpp
+++ file.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+
+// some math 
 int square(int x) {
     return x * x;
 }
 int main() {
-    std::cout << "Square of 3 is " << square(3) << std::endl;
+
+    std::cout << "the Square root of 3 is " << square(3) << std::endl;
     return 0;
 }
